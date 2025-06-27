c2f6543b9b86e657923d3522347c4836213238e8
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-06-27 12:05:34 +0200

    change import

--- bot.ts
+++ bot.ts
@@ -1,6 +1,6 @@
 // bot.ts
 import { Bot } from "https://deno.land/x/grammy@v1.36.3/mod.ts";
-import "https://deno.land/std@0.224.0/dotenv/load.ts";
+
 
 const bot = new Bot(Deno.env.get("BOT_TOKEN")!); // safer than hardcoding token
 
