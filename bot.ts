b71674d0c5bdd36ad88909691f5b601e81815472
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-06-27 11:24:19 +0200

    change bot.ts

--- bot.ts
+++ bot.ts
@@ -4,6 +4,6 @@ import { Bot } from "https://deno.land/x/grammy@v1.36.3/mod.ts";
 const bot = new Bot(Deno.env.get("BOT_TOKEN")!); // safer than hardcoding token
 
 bot.command("start", (ctx) => ctx.reply("Welcome!"));
-bot.command("hello", (ctx) => ctx.reply("Hi there!"));
+
 
 export default bot;
