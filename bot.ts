8d84b45f318586d95751d0a54f8cf1e88394e6cb
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-06-28 10:41:10 +0200

    bot change 2

--- bot.ts
+++ bot.ts
@@ -1,4 +1,3 @@
-// bot.ts
 import { Bot } from "https://deno.land/x/grammy@v1.36.3/mod.ts";
 
 const bot = new Bot(Deno.env.get("BOT_TOKEN")!);
@@ -7,9 +6,10 @@ bot.command("start", async (ctx) => {
   const username = ctx.from?.first_name || "there";
 
   await ctx.replyWithPhoto("https://crownslots.pro/img/bot.jpg", {
-    caption: `Hello ${username}, Fordern Sie Ihre BONUSSE an 🎁\n👉 [Jetzt ansehen](https://t.me/slotalarmcasinos_bot/slots)`,
-    parse_mode: "Markdown",
+    caption: `Hello ${username}, Fordern Sie Ihre BONUSSE an 🎁<br><a href="https://t.me/slotalarmcasinos_bot/slots">👉 Jetzt ansehen</a>`,
+    parse_mode: "HTML",
   });
 });
 
 export default bot;
+
